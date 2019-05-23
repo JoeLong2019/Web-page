ee1e68971f1f11deaf1dd56d1d23cdd02e28782b
JoeLong2019 <50843137+JoeLong2019@users.noreply.github.com>
2019-05-23 16:29:54 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var cell =  document.getElementById("something")
+console.log(cell)
+
+cell.addEventListener("click", getPlayerMove)
+
+function getPlayerMove(){
+    var moveNum = X
+    console.log(moveNum)
+}
