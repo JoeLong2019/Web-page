5b97ff07767ee09471172d0273c86cacb5d21752
JoeLong2019 <50843137+JoeLong2019@users.noreply.github.com>
2019-05-28 08:20:45 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-var grid = []; // set grid 
+var grid = []; // 
 grid[0] = [];
 grid[1] = [];
 grid[2] = [];
 
-const cells = document.querySelectorAll(".cell")
+const cells = document.querySelectorAll(".cell") // Returns all element descendants of node that match selectors
 
 cells.forEach(function(cell) {
-    cell.addEventListener("click", function(e) {
-        cell.innerHTML = "X";
+    cell.addEventListener("click", function(e) { // when cell is clicked
+        cell.innerHTML = "X"; // adds X
 
-        while next("click")
-        cells.forEach(function(cell) {
-            cell.addEventListener("click", function(e) {
-                cell.innerHTML = "O";
+        cells.forEach(function() {
+            cell.addEventListener("click", function(e) { // when clicked
+                cell.innerHTML = "O"; // adds O
+                
+                cells.forEach(function(O) {
+                    cell.addEventListener("click", function(e) { // when clicked
+                        cell.innerHTML = "X"; // adds  X
+
+            })
+        });  
     })
 });        
     })
 });
+
 function restart() {
-    document.location.href = "Restart";
+    document.location.href=""; 
 }
